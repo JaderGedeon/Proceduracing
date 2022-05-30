fa65977608a8fdd414211508e5bf81d41d975edb
Jader <jadergedeon@gmail.com>
2022-05-30 09:41:32 -0300

    Clock

---
+++ TCC - Proceduracing/Assets/CustomMaskUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class CustomMaskUI : Image
+{
+    public override Material materialForRendering 
+    {
+        get {
+            Material result = Instantiate(base.materialForRendering);
+            result.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return result;
+        }
+    }
+}
